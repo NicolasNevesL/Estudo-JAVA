f34359000669bf12e0b0a0b45180bfecbf398fdb
Nicolas Neves <nicolasneveslouzada@gmail.com>
2026-07-02 10:32:48 -0300

    Aprendido comentarios

--- src/devdojo/estudos/maratonajava/introducao/OlaNicolas.java
+++ src/devdojo/estudos/maratonajava/introducao/OlaNicolas.java
@@ -1,7 +1,16 @@
-package devdojo.estudos.maratonajava.introdução;
+package devdojo.estudos.maratonajava.introducao;
 
+/**
+ * javadoc
+ */
 public class OlaNicolas {
+    // Esse é um comentário de uma linha :)
+    /*
+    Esse é um
+    comentário de
+    multiplas linhas
+     */
     public static void main(String[]args){
-        System.out.println("Olá, Mundo");
+        System.out.println("Olá, Mundo!");
     }
 }
