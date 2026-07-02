7f10eb826763390c63eb28ed7669d557f698e15b
Nicolas Neves <nicolasneveslouzada@gmail.com>
2026-07-02 10:34:27 -0300

    Convencoes de Variaveis

--- src/devdojo/estudos/maratonajava/introducao/TiposPrimitivos.java
+++ src/devdojo/estudos/maratonajava/introducao/TiposPrimitivos.java
@@ -1,4 +1,9 @@
 package devdojo.estudos.maratonajava.introducao;
 
 public class TiposPrimitivos {
+    public static void main(String[]args){
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        System.out.println("A idade é "+idade+" anos");
+    }
 }
