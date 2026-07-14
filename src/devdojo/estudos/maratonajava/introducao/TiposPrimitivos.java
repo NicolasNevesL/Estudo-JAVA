da698fdccd4489d34781011428dfabd25daf8d20
Nicolas Neves <nicolasneveslouzada@gmail.com>
2026-07-14 08:56:56 -0300

    Aprendido casting e String

--- src/devdojo/estudos/maratonajava/introducao/TiposPrimitivos.java
+++ src/devdojo/estudos/maratonajava/introducao/TiposPrimitivos.java
@@ -4,16 +4,18 @@ public class TiposPrimitivos {
     public static void main(String[]args){
         // int, double, float, char, byte, short, long, boolean
         int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        long numeroGrande = 100000L;
+        double salarioDouble = 2000D;
+        float salarioFloat = 2500F;
         byte idadeByte = 10;
         short idadeShort = 10;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = '\u0041';
+        String nome = "Camila";
         System.out.println("A idade é "+idade+" anos");
         System.out.println(falso);
         System.out.println("char "+caractere);
+        System.out.println("Seu nome é "+nome);
     }
 }
