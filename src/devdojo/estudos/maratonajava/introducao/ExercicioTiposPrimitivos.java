ffe943060ae722ec100d7722e7e652be0aa7a3fa
Nicolas Neves <nicolasneveslouzada@gmail.com>
2026-07-14 09:43:32 -0300

    Concluido exercício de Tipos Primitivos

---
+++ src/devdojo/estudos/maratonajava/introducao/ExercicioTiposPrimitivos.java
@@ -0,0 +1,12 @@
+package devdojo.estudos.maratonajava.introducao;
+
+public class ExercicioTiposPrimitivos {
+    public static void main (String[] args){
+        String nome = "Carlos";
+        String endereço = "Rua das Flores, n67";
+        String data = "29/10/1997";
+        float salario = 2500.00F;
+
+        System.out.println("Eu "+nome+", morando no endereço "+endereço+", confirmo que recbi o salario de "+salario+ ", na data "+data);
+    }
+}
