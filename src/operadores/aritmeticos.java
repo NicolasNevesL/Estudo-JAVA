71ce7ff405896f15e214ee271b971209a86f234d
Nicolas Neves <nicolasneveslouzada@gmail.com>
2026-07-15 09:06:23 -0300

    Aprendido Operadores Aritmeticos

---
+++ src/operadores/aritmeticos.java
@@ -0,0 +1,26 @@
+package operadores;
+
+public class aritméticos {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        int resultadoSoma = numero02 + numero01;
+        System.out.println(resultadoSoma);
+
+        int numero03 = 10;
+        int numero04 = 20;
+        int resultadoMultiplicacao = numero03 * numero04;
+        System.out.println(resultadoMultiplicacao);
+
+        int numero05 = 10;
+        int numero06 = 20;
+        int resultadoSubtracao = numero06 - numero05;
+        System.out.println(resultadoSubtracao);
+
+        int numero07 = 10;
+        double numero08 = 20;
+        double resultadoDivisao = numero07 / numero08;
+        System.out.println(resultadoDivisao);
+    }
+}
