25fc83a0f391be9843c14cd5a6dbf57ba7b4f56f
Nicolas Neves <nicolasneveslouzada@gmail.com>
2026-07-27 09:39:42 -0300

    Visto While, Do-While e For

---
+++ src/estruturasderepeticao/WhileDoWhileFor.java
@@ -0,0 +1,20 @@
+package estruturasderepeticao;
+
+public class WhileDoWhileFor {
+    public static void main(String[] args) {
+        // While, Do While, For
+
+        int count = 0;
+        while (count < 10) {
+            System.out.println(++count);
+        }
+        count = 0;
+        do {
+            System.out.println("Dentro do Do-While "+ ++count);
+        } while (count < 10);
+
+        for (int i=1;i < 10; i++){
+            System.out.println("For "+i);
+        }
+    }
+}
