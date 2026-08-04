6c5248881d2e7f4ab3022e1c3e99995a7a79c3f7
Nicolas Neves <nicolasneveslouzada@gmail.com>
2026-08-04 08:24:51 -0300

    Visto Arrays pt. 1

---
+++ src/arrays/ArraysPT1.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class ArraysPT1 {
+    public static void main(String[] args) {
+        int [] idades = new int[3];
+        idades[0] = 12;
+        idades[1] = 33;
+        idades[2] = 22;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
