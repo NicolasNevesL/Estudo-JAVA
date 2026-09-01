20608e454c73659ce356aa3e67d39f2b574c47a9
Nicolas Neves <nicolasneveslouzada@gmail.com>
2026-09-01 11:00:28 -0300

    Visto Método Estático

--- poo/modificadorDeAcesso/Test/CarroTest01.java
+++ poo/modificadorDeAcesso/Test/CarroTest01.java
@@ -4,13 +4,11 @@ import modificadorDeAcesso.dominio.Carro;
 
 public class CarroTest01 {
     public static void main(String[] args) {
+        Carro.setVelocidadeLimite(180);
+
         Carro c1 = new Carro("BMW", 290);
         Carro c2 = new Carro("BMW", 270);
         Carro c3 = new Carro("BMW", 275);
-
-        System.out.println(Carro.velocidadeLimite);
-        Carro.velocidadeLimite = 180;
-        System.out.println(Carro.velocidadeLimite);
 
         c1.imprime();
         c2.imprime();
